b28faff6478a85db240abeb25ded3277f9446141
pavithra200595 <pavipavi491@gmail.com>
2023-03-06 20:52:27 +0530

    First commit

---
+++ studentDetails.java
@@ -0,0 +1,16 @@
+class studentDetails
+{
+    
+/**
+ * @param args
+ */
+public static void main(String args[])
+{
+    String name="Akash",std="X",sec="A";
+    double eng=90,tam=98,mat=97,sci=96,soc5=96,tot,avg;
+    System.out.println("Student Name: "+name);
+    System.out.println("Stsandard: "+std);
+    System.out.println("Section: "+sec);
+
+}
+}
