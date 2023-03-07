db960fe1f2f66f4a4364bedfe6f70faa7e17fcf0
pavithra200595 <pavipavi491@gmail.com>
2023-03-07 11:25:11 +0530

    Dispaying marks

--- studentDetails.java
+++ studentDetails.java
@@ -7,10 +7,15 @@ class studentDetails
 public static void main(String args[])
 {
     String name="Akash",std="X",sec="A";
-    double eng=90,tam=98,mat=97,sci=96,soc5=96,tot,avg;
+    double eng=90,tam=98,mat=97,sci=96,soc=96,tot,avg;
     System.out.println("Student Name: "+name);
     System.out.println("Stsandard: "+std);
     System.out.println("Section: "+sec);
+    System.out.println("English"+eng);
+    System.out.println("Tamil:"+tam);
+    System.out.println("Maths:"+mat);
+    System.out.println("Science:"+sci);
+    System.out.println("Social"+soc);
 
 }
 }
