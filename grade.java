7b08fe8cf4bd39a79c6da59eab0c0135df845b9c
nisha <nishasuryasiva@gmail.com>
2023-03-16 16:15:57 +0530

    first

--- grade.java
+++ grade.java
@@ -2,6 +2,8 @@ public class grade {
     public static void main(String args[])
     {
         System.out.println("grade");
+          System.out.println("mark");
+            System.out.println("sub");
     }
     
 }
